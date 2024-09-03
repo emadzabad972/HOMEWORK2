9e6df15a04c063cf643203bbe87b05fa72bf279d
emadzabad972 <emadzabad972@gmail.com>
2024-09-03 21:47:44 +0300

    Add files via upload

---
+++ HW2part3.py
@@ -0,0 +1,12 @@
+# start
+
+f_name: str = input("what's your first name?")
+
+l_name: str = input("what's your last name?")
+
+print('*'+f_name+'*'+l_name+'*')
+
+print('-'+f_name+'-'+l_name+'-')
+
+# end
+
