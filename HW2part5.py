d969741df7f7d85b371cfe810bfe20bd3671ba09
emadzabad972 <emadzabad972@gmail.com>
2024-09-03 23:19:00 +0300

    Add files via upload

---
+++ HW2part5.py
@@ -0,0 +1,17 @@
+# start
+
+a: int = int(input('type a random number:'))
+
+b: int = int(input('type another random number:'))
+
+if a > b:
+    print(b)
+    print(a)
+else:
+    print(a)
+    print(b)
+
+# end
+
+
+
