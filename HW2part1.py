b4dd37ff8970ad1f8a5673ed3b75d009d73df9ea
emadzabad972 <emadzabad972@gmail.com>
2024-09-03 20:41:54 +0300

    Add files via upload

---
+++ HW2part1.py
@@ -0,0 +1,18 @@
+# START
+
+x: int = int(input('type a first number:'))
+
+y: int = int(input('type a second number:'))
+
+z: int = int(input('type a third number:'))
+
+plus: int = x + y + z
+
+mult: int = x * y * z
+
+print(plus)
+
+print(mult)
+
+# end
+
